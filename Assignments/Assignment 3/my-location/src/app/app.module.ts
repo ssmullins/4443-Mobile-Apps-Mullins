6e3beabc7bd949ee17c7b56ee3aba00f53cfe61f
shedwreck <samuel.s.mullins@gmail.com>
2018-10-10 15:32:18 -0500

    Creating Assignments Folder. Adding Assignment 3.

---
+++ Assignments/Assignment 3/my-location/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { FIREBASE_CONFIG } from './firebase'
+import { AngularFirestoreModule } from 'angularfire2/firestore'
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { DataProvider } from '../providers/data/data';
+import { Geolocation } from '@ionic-native/geolocation';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider
+  ]
+})
+export class AppModule {}
