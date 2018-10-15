fd976260db7a25a716ce86a93c2a320e3f751a63
shedwreck <samuel.s.mullins@gmail.com>
2018-10-15 15:14:01 -0500

    small changes in data.ts

--- Assignments/Assignment 3/Part_2/my-location/src/app/firebase.ts
+++ Assignments/Assignment 3/Part_2/my-location/src/app/firebase.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDBqx8joTKXN2J50k3NJL-GgKodvxtz-ig",
-    authDomain: "my-location-6298c.firebaseapp.com",
-    databaseURL: "https://my-location-6298c.firebaseio.com",
-    projectId: "my-location-6298c",
-    storageBucket: "my-location-6298c.appspot.com",
-    messagingSenderId: "732442500418"
+    apiKey: "AIzaSyAesyG2sjUJJMTl6Ln-l2kTGUsnGefSHIc",
+    authDomain: "my-location-1419c.firebaseapp.com",
+    databaseURL: "https://my-location-1419c.firebaseio.com",
+    projectId: "my-location-1419c",
+    storageBucket: "my-location-1419c.appspot.com",
+    messagingSenderId: "266284588805"
 } 
